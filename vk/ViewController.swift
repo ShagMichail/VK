6f400754149acda4dd86374de50b6f960b2efe43
ShagMichail <shiga2001@mail.ru>
2023-05-05 15:07:39 +0300

    Initial Commit

---
+++ vk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  vk
+//
+//  Created by Михаил Шаговитов on 05.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
